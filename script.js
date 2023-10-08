abb8cb5e2a8e4d000aa6cb9cc537416a82929400
villhame <109059898+villhame@users.noreply.github.com>
2023-10-08 14:15:50 +1100

    Not sure what was done last time

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 // script.js
 const gridContainer = document.querySelector('.Grid')
 
+// const slider = document.getElementsByClassName('slider');
+// const sliderValue = document.getElementsByClassName('sliderValue');
+
+// // Function to update the value display span
+// function updateSliderValue() {
+//     sliderValue.textContent = slider.value;
+// }
+
+// // Add an event listener to the slider input
+// slider.addEventListener('input', updateSliderValue);
+
+// Initial update of the value display
+
+
 function createGrid(rows, cols) {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
@@ -14,5 +28,7 @@ function createGrid(rows, cols) {
 }
 
 createGrid(16, 16);
+updateSliderValue();
+
 
 
